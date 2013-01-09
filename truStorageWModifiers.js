1985a485ee1e23c677ccee1d5c0b5c3f7dcdd9a9
andresgallo <nyandresg@hotmail.com>
2013-01-09 00:29:22 -0500

    new

---
+++ truStorageWModifiers.js
@@ -0,0 +1,57 @@
+truStorage = {   
+	makeFormat : function(str){
+		var formatted = str;
+		try { formatted = JSON.parse(str);} 
+		catch (e){ console.log('truStorage not passed a json object');}
+		return formatted;
+	},
+	readLocalObj : function(str,setVal,modifier){
+		if(typeof setVal === "object")setVal = JSON.stringify(setVal);
+        if(typeof modifier === "undefined")modifier = false;
+
+		var strArr = str.split('.'),
+		    strArrLn = strArr.length,
+			currentLevel = localStorage,//Depth level starts at localStorageRoot
+			rootKey = strArr[0];
+		var objCopy = {},
+			isSetMode = false;
+
+		if(typeof setVal !== "undefined")isSetMode = true;
+
+		for(var i=0;i<strArrLn;i++){
+			var key = strArr[i];
+
+			if(typeof currentLevel === "undefined" ||
+				typeof currentLevel !== "object")console.warn('cannot append key to non object');
+            
+            
+			if(isSetMode && i === strArrLn -1){
+                 if(modifier){
+                    var r = this.makeFormat(currentLevel[key]);
+                    r[modifier].apply(r,JSON.parse(setVal));
+console.warn(r);    setVal = r;
+                 }currentLevel[key] = setVal;	
+            }
+
+			//Generate objects if needed
+			if( !(typeof currentLevel === "object" && key in currentLevel) ){
+				if(i !== strArrLn -1)currentLevel[key] = {};
+				else currentLevel[key] = null;
+			}
+
+			currentLevel = this.makeFormat(currentLevel[key]);
+			if(i===0)objCopy = currentLevel;
+		}
+		if(isSetMode)localStorage[rootKey] = JSON.stringify(objCopy);
+
+		return currentLevel;
+	},
+	setItem : function(str, value, modifier){
+		var fetchPath = this.readLocalObj(str,value,modifier);
+		return fetchPath;
+	},
+	getItem : function(str){
+		var fetchPath = this.readLocalObj(str);
+		return fetchPath; 
+	}
+}
